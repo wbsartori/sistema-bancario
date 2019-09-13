e4f42a551dee0116915767c3beed7c6e965459e5
Wesley Sartori <wbsartori@gmail.com>
2019-09-12 23:10:46 -0300

    Menus Finalizados

--- controllers/controllerConta.py
+++ controllers/controllerConta.py
@@ -1,10 +1,10 @@
 from models import conta
-
-class ControllerConta:
+from models import pessoa
+class ControllerConta(Pessoa, ContaCorrente, ContaPoupanca):
     def sacar(self, valor):
         pass
 
-    def depositar(self, valor):
+    def depositar(self,nAgencia, nConta, valor):
         pass
 
     def transferir(self, numeroConta, agencia, valor):
--- main.py
+++ main.py
@@ -17,53 +17,128 @@ while 1:
     clear()
     result = Figlet(font='doom')
     print(result.renderText('LadroBank'))
-    print(' # Operações #')
-
+    print('# Operações Disponíveis #')
+    print('')
     menuPrincipal = [' Sacar',' Depositar',' Transferir',' Saldo',' Sair']
     items = 1
     for mp in menuPrincipal:
         print( str(items) + ' - ' + mp)
         items = items + 1
-
     print('')
-    opcoesPrincipal = int(input('Seleciona a operação: '))
-
-
+    opcoesPrincipal = int(input('Selecione a operação: '))
+    print('')
     if opcoesPrincipal == 1:
         clear()
         ms = Figlet(font='doom')
-        print(ms.renderText('Saque Bancário'))
-        print(' # Operações #')
-
-        menuSacar = ['Inserir Valor',' Voltar']
+        print(ms.renderText('Saques'))
+        print('# Operações Disponíveis #')
+        print('')
+        menuSacar = ['Sacar','Menu Principal']
         items = 1
         for mSacar in menuSacar:
             print(str(items) + ' - ' + mSacar)
             items = items + 1
-
-        saqueOP =  int(input('Operação: '))
+        print('')
+        saqueOP = int(input('Operação: '))
+        print('')
         if saqueOP == 1:
             saque = int(input('Valor a sacar: '))
         else:
-            break
+            print('Finalizando Operação de Saque.')
+            time.sleep(1)
+            print('Obrigado por utilizar nossos serviços.')
+            time.sleep(0.8)
 
     elif opcoesPrincipal == 2:
-        print('Depositar')
-        break
+        clear()
+        ms = Figlet(font='doom')
+        print(ms.renderText('Depositos'))
+        print('# Operações Disponíveis #')
+        print('')
+        menuDepositar = ['Depositar', 'Menu Principal']
+        items = 1
+        for mDeposito in menuDepositar:
+            print(str(items) + ' - ' + mDeposito)
+            items = items + 1
+        print('')
+        depositoOP = int(input('Operação: '))
+        print('')
+        if depositoOP == 1:
+            conta = int(input('Conta: '))
+            agencia = int(input('Agencia: '))
+            deposito = int(input('Valor a depositar: '))
+        else:
+            print('Finalizando Operação de Depósito.')
+            time.sleep(1)
+            print('Obrigado por utilizar nossos serviços.')
+            time.sleep(0.8)
+
     elif opcoesPrincipal == 3:
-        print('Transferir')
-        break
+        clear()
+        ms = Figlet(font='doom')
+        print(ms.renderText('Transferencias'))
+        print('# Operações Disponíveis #')
+        print('')
+        menuTransferir = ['Transferir', 'Menu Principal']
+        items = 1
+        for mTransferir in menuTransferir:
+            print(str(items) + ' - ' + mTransferir)
+            items = items + 1
+        print('')
+        depositoOP = int(input('Operação: '))
+        print('')
+        if depositoOP == 1:
+            conta = int(input('Conta: '))
+            agencia = int(input('Agencia: '))
+            deposito = int(input('Valor a transferir: '))
+        else:
+            print('Finalizando Operação de Transferência.')
+            time.sleep(1)
+            print('Obrigado por utilizar nossos serviços.')
+            time.sleep(0.8)
+
+
     elif opcoesPrincipal == 4:
-        print('Saldo')
-        break
+        clear()
+        ms = Figlet(font='doom')
+        print(ms.renderText('Saldos'))
+        print(' # Operações #')
+        print('')
+        menuSaldo = ['Ver Saldo', 'Menu Principal']
+        items = 1
+        for mSaldo in menuSaldo:
+            print(str(items) + ' - ' + mSaldo)
+            items = items + 1
+        print('')
+        depositoOP = int(input('Operação: '))
+        print('')
+        if depositoOP == 1:
+            print('Saldo = R$ 5000,00')
+            time.sleep(1)
+        else:
+            print('Finalizando Operação de Saldo.')
+            time.sleep(1)
+            print('Obrigado por utilizar nossos serviços.')
+            time.sleep(0.8)
+
     elif opcoesPrincipal == 5:
-        print('Obrigado por utilizar nossos serviços!')
-        print('...' + time.sleep(1) + str('...') + time.sleep(1) + str('...') + time.sleep(1))
+        i = ' '
+        print('')
+        print('Finalizando Sistema', end='')
+        time.sleep(1)
+
+        for p in range(5):
+            print(i)
+            time.sleep(0.5)
+
+        print('Obrigado por utilizar nossos serviços.')
+        print('')
+        msg = Figlet(font='doom')
+        print(msg.renderText('LadroBank Agradece !'))
+        time.sleep(0.8)
         break
 
     else:
         print('Operação não existe, tente novamente!')
-        print('...')
-        print('...')
-        print('...')
+        time.sleep(1.5)
         clear()
